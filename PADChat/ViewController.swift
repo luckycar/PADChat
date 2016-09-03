7c866588f93f7c2b63d19cc8781e6a163f354f58
KuanHaoChen <KuanHaoChen@chenguanhaode-macbook-air.local>
2016-09-03 22:53:22 +0800

    Initial Commit

---
+++ PADChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PADChat
+//
+//  Created by KuanHaoChen on 2016/9/3.
+//  Copyright © 2016年 KuanHaoChen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
